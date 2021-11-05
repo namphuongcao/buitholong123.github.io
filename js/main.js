ada29fefa7706f4cd9a89aebb0fb586e2c9e102f
BuiThoLong <56524745+BuiThoLong@users.noreply.github.com>
2021-11-05 10:44:46 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/lookMe1.jpg',
+        imageUrl: 'img/.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
